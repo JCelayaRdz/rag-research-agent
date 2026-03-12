5878ac2b3b72d96e14f022c656cd0c91552e5a50
JCelayaRdz <jcelayardz@gmail.com>
2026-03-12 22:08:17 +0100

    add log level to config

--- services/main-api/config.py
+++ services/main-api/config.py
@@ -1,13 +1,17 @@
 from pydantic_settings import BaseSettings
 from pydantic import PostgresDsn, Field
-from pydantic import model_validator
+from pydantic import model_validator, computed_field
 from typing import Literal, Self
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(BaseSettings):
     DATABASE_URL: PostgresDsn | None = None
     API_VERSION: str = "1.0"
     ENVIRONMENT: Literal["LOCAL", "PRODUCTION"]
     DATABASE_PARAMETER_NAME: str | None = None
+    LOG_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
     API_PORT: int = Field(ge=1024, le=49151)
 
     @model_validator(mode="after")
@@ -17,5 +21,17 @@ class Config(BaseSettings):
         if self.ENVIRONMENT == "PRODUCTION" and not self.DATABASE_PARAMETER_NAME:
             raise ValueError("'DATABASE_PARAMETER_NAME' not set in production environment")
         return self
+    
+    @computed_field
+    @property
+    def LOG_LEVEL_PARSED(self) -> int:
+        level_mappings = {
+            "DEBUG": 10,
+            "INFO": 20,
+            "WARNING": 30,
+            "ERROR": 40,
+            "CRITICAL": 50
+        }
+        return level_mappings.get(self.LOG_LEVEL, 20)
     
 settings = Config()
