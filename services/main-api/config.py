3118ef986bd8e075d9790285c6cf71b956598972
JCelayaRdz <jcelayardz@gmail.com>
2026-03-12 23:07:29 +0100

    add support for docker environment in config

--- services/main-api/config.py
+++ services/main-api/config.py
@@ -1,23 +1,35 @@
-from pydantic_settings import BaseSettings
-from pydantic import PostgresDsn, Field
-from pydantic import model_validator, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import (
+    PostgresDsn,
+    Field,
+    model_validator, 
+    computed_field
+)
 from typing import Literal, Self
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config(BaseSettings):
-    DATABASE_URL: PostgresDsn | None = None
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        secrets_dir="/run/secrets"
+    )
+
     API_VERSION: str = "1.0"
-    ENVIRONMENT: Literal["LOCAL", "PRODUCTION"]
-    DATABASE_PARAMETER_NAME: str | None = None
+    ENVIRONMENT: Literal["LOCAL", "DOCKER", "PRODUCTION"]
     LOG_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
     API_PORT: int = Field(ge=1024, le=49151)
+
+    DATABASE_URL: PostgresDsn | None = None
+    DATABASE_PARAMETER_NAME: str | None = None
+    db_password: str | None = None
 
     @model_validator(mode="after")
     def validate_database_url(self) -> Self:
         if self.ENVIRONMENT == "LOCAL" and not self.DATABASE_URL:
             raise ValueError("'DATABASE_URL' not set in local environment")
+        if self.ENVIRONMENT == "DOCKER":
+            if not self.db_password:
+                raise ValueError("'db_password' secret not found")
+            self.DATABASE_URL = f"postgresql://postgres:{self.db_password}@database:5432/research-assistant"
         if self.ENVIRONMENT == "PRODUCTION" and not self.DATABASE_PARAMETER_NAME:
             raise ValueError("'DATABASE_PARAMETER_NAME' not set in production environment")
         return self
