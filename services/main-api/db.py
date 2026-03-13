fe64cc7de4044d586d745eb244db984a706aea6e
JCelayaRdz <jcelayardz@gmail.com>
2026-03-13 20:42:40 +0100

    fix typo in import

--- services/main-api/db.py
+++ services/main-api/db.py
@@ -1,16 +1,16 @@
 from psycopg import Connection
-from psycopg_pool import AsynConnectionPool
+from psycopg_pool import AsyncConnectionPool
 from config import settings
 from fastapi import Request
 from typing import cast, AsyncGenerator
 
-def get_db_connection_pool() -> AsynConnectionPool:
-    return AsynConnectionPool(
+def get_db_connection_pool() -> AsyncConnectionPool:
+    return AsyncConnectionPool(
         conninfo=settings.DATABASE_URL.unicode_string(),
         open=False
     )
 
 async def db_conn(request: Request) -> AsyncGenerator[Connection, None]:
-    db_pool = cast(AsynConnectionPool, request.state.db_pool)
+    db_pool = cast(AsyncConnectionPool, request.state.db_pool)
     async with db_pool.connection() as conn:
         yield conn
